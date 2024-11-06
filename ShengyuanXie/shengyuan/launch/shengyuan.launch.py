b8111440ee2b6c54d354ae66ca73c2ca12a0668e
Shengyuan Xie <138834215+LittleFlipped@users.noreply.github.com>
2024-11-06 12:05:06 +1100

    miros

---
+++ ShengyuanXie/shengyuan/launch/shengyuan.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='shengyuan',
+            executable='node1',
+            name='node1'
+        ),
+
+        # customize launch file below
+
+        # end launch file customization
+    ])
